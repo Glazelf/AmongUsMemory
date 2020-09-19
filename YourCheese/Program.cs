88db5d15d887cc98412b172acfcb2e4d2a3dee59
Glazelf <gideon.steenbergen@gmail.com>
2020-09-19 14:29:14 +0200

    reorder table, add role column

--- YourCheese/Program.cs
+++ YourCheese/Program.cs
@@ -18,17 +18,20 @@ namespace YourCheese
             while (true)
             {
                 Console.Clear();
-                PrintRow("offset", "Name", "OwnerId", "PlayerId", "Color", "spawnid", "spawnflag");
+                PrintRow("offset", "Name", "Color", "Role", "OwnerId", "PlayerId", "spawnid", "spawnflag");
                 PrintLine();
 
                 foreach (var data in playerDatas)
                 {
+                    var roleName = "Crewmate";
                     if (data.IsLocalPlayer)
                         Console.ForegroundColor = ConsoleColor.Green;
                     if (data.PlayerInfo.Value.IsDead == 1)
                         Console.ForegroundColor = ConsoleColor.Red;
-                    if (data.PlayerInfo.Value.IsImpostor == 1)
-                        Console.ForegroundColor = ConsoleColor.Blue;
+                    if (data.PlayerInfo.Value.IsImpostor == 1) {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        roleName = "Impostor";
+                    }
 
                     var colorName = "";
                     switch (data.PlayerInfo.Value.ColorId)
@@ -71,7 +74,7 @@ namespace YourCheese
                             break;
                     }
                     var Name = HamsterCheese.AmongUsMemory.Utils.ReadString(data.PlayerInfo.Value.PlayerName);
-                    PrintRow($"{(data.IsLocalPlayer == true ? "Me->" : "")}{data.offset_str}", $"{Name}", $"{data.Instance.OwnerId}", $"{data.Instance.PlayerId}", $"{colorName}", $"{data.Instance.SpawnId}", $"{data.Instance.SpawnFlags}");
+                    PrintRow($"{(data.IsLocalPlayer == true ? "Me->" : "")}{data.offset_str}", $"{Name}", $"{colorName}", $"{roleName}", $"{data.Instance.OwnerId}", $"{data.Instance.PlayerId}", $"{data.Instance.SpawnId}", $"{data.Instance.SpawnFlags}");
                     Console.ForegroundColor = ConsoleColor.White;
                     PrintLine();
                 }
